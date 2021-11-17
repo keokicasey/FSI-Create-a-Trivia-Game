ad0b720e85943fe0d9d7a394f7ede3d8ae088097
Keoki <keoki.alan.mundon@gmail.com>
2021-11-16 19:18:48 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your Code Here
+let userName = window.prompt('Please enter your name: ')
+let userScore = 0;
+
+for (let i = 0; i < questions.length; i++) {
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer) {
+        userScore = userScore + 10
+    }
+}
+
+window.alert('Your score is: '+userScore)
+
+let serializedGames = localStorage.getItem('games')
+
+let games = []
+if(serializedGames === null) {
+    games = []
+} else {
+    games = JSON.parse(serializedGames)
+}
+
+let game = {
+    user: userName,
+    score: userScore
+}
+
+games.push(game)
+
+console.log(games)
+
+serializedGames = JSON.stringify(games)
+localStorage.setItem('games', serializedGames)
